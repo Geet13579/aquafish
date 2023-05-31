240c0b41d11cef61add4427f37c2bbe3c1faab43
Yuvraj <eryuvrajkhare08@gmail.com>
2023-05-31 18:51:10 +0530

    todays

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,49 @@
 import React from 'react';
 import '../css/header.css';
-import { useEffect,useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
+import menu from '../Images/menu.svg'
+import { hover } from '@testing-library/user-event/dist/hover';
 
 function Header() {
+  const [width, setWidth] = useState(window.innerWidth)
+
+  const [isMobile, setIsMobile] = useState(false)
+
+
+
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+
+      setWidth(window.innerWidth)
+    });
+
+    console.log(window.innerWidth)
+
+
+  });
+
+  const openPopupmenu = () => {
+    setIsMobile(true)
+  }
+  const closePopupmenu = () => {
+    setIsMobile(false)
+  }
+
+
 
 
   return (
+
     <div>
+
+      {isMobile == true ?
+
+        <div className='popuMenu'>
+          <div onClick={closePopupmenu} className="closeButton" >close</div>
+
+        </div>
+        : <div></div>}
+
       <div className='Header_Main'>
         <div className='leftnavbar'>
 
@@ -17,62 +54,72 @@ function Header() {
 
 
         </div>
-        <div className='rightnavbar'>
-          <div className='right_menu'>
-            <a href='#/' className="hover_style">
-              Home
-            </a>
 
-
-          </div>
-
-
-
-
-          <div className='right_menu'>
-            <a href='#/' className="hover_style">
-              About
-            </a>
-
-
-
-
-          </div>
-          <div className='right_menu'>
-            <a href='#/' className="hover_style">
-              Service
-
-            </a>
-
-
-
-          </div>
-          <div className='right_menu'>
-            <a href='#/' className="hover_style">
-              Gallary
-            </a>
-
-
-
-
-          </div>
-          <div className='right_menu'>
-            <a href='#/' className="hover_style">
-              Contact
-
-            </a>
-
-
-
-          </div>
+        {width <= 457 ? <div className='rightnavbar' style={{ justifyContent: "flex-end", marginRight: "30px" }}>
+          <div ><img onClick={openPopupmenu} src={menu} className="menuStyle" /></div>
 
 
         </div>
 
+          :
+          <div className='rightnavbar'>
+            <div className='right_menu'>
+              <a href='#/' className="hover_style">
+                Home
+              </a>
+
+
+            </div>
+
+
+
+
+            <div className='right_menu'>
+              <a href='#/' className="hover_style">
+                About
+              </a>
+
+
+
+
+            </div>
+            <div className='right_menu'>
+              <a href='#/' className="hover_style">
+                Service
+
+              </a>
+
+
+
+            </div>
+            <div className='right_menu'>
+              <a href='#/' className="hover_style">
+                Gallary
+              </a>
+
+
+
+
+            </div>
+            <div className='right_menu'>
+              <a href='#/' className="hover_style">
+                Contact
+
+              </a>
+
+
+
+            </div>
+          </div>
+        }
+
 
       </div>
 
+
     </div>
+
+
   )
 }
 
--- src/Components/MainContainer.js
+++ src/Components/MainContainer.js
@@ -6,9 +6,14 @@ import { useState } from 'react';
 import fish_img from '../Images/fish_img.webp'
 
 import fish1 from '../Images/fish1.webp'
+import fishMain from '../Images/fish_main2.jpg';
+import Typewriter from 'typewriter-effect';
+
+
 
 // import Footer from '../Components/Footer.js';
 function MainContainer() {
+
 
     const [display1, changeStyleForSlider] = useState("block");
     const [display2, changeStyleForSlider1] = useState("none");
@@ -48,20 +53,91 @@ function MainContainer() {
         < div >
 
             <Header />
-            <div className='Conatiner'>
-
-                {/* <div style={{ backgroundImage: `url(${fish_img})`, height: "100px", width: "100px" }}>  </div> */}
+            <div className='Conatiner' style={{ backgroundImage: "linear-gradient( #F29053,#42c4dc)" }}>
 
 
-                <div className='image_container'>
+                <div className='frontContainer'>
+                    <div className='image_container'>
+                        <div className='img_animate' style={{ backgroundImage: `url(${fishMain})`, height: "100%", width: "100%", backgroundSize: "cover", backgroundRepeat: "no-repeat" }}>  </div>
+                        {/* <div className='circleDiv x1'>
+                        1
+                    </div>
+                    <div className='circleDiv x2'>
+                        2
+                    </div>
+                    <div className='circleDiv x3'>
+                        3
+                    </div>
+                    <div className='circleDiv x4'>
+                        4
+                    </div>
+                    <div className='circleDiv x5'>
+                        5
+                    </div>
+                    <div className='circleDiv x6'>
+                        6
+                    </div> */}
 
+
+                        {/* <div style={{ backgroundImage: `url(${fishMain})`, height: "100%", width: "100%", backgroundSize: "contain", backgroundRepeat: "no-repeat" }}>  </div> */}
+                    </div>
+
+                    {/* <Typewriter
+                        options={{
+
+                            autoStart: true,
+                            loop: true,
+                        }}
+                        onInit={(typewriter) => {
+                            typewriter.pauseFor(1000)
+                                .typeString('<h1 style="color: #DC2D55; font: normal normal 700 35px Poppins";>The all new way of</h1>')
+
+
+                                .typeString('<h1 style="color:#141923 ;font: normal normal 700 40px Poppins";>Tiffin Delivery... </h1>')
+                                //.typeString('<h1 style="color:#DC2D55;font: normal normal 700 50px/30px Poppins";>...<span style="color:black">|</span></h1>')
+
+                                // .deleteAll()
+                                .typeString('<h1 style="color:#DC2D55;font: normal normal 700 35px Poppins";>Reimagined avatar<h1>')
+                                .typeString('<h1 style="color:#DC2D55;font: normal normal 700 35px Poppins";> of <span style="color:#141923 ;font: normal normal 800 35px Poppins";>combos</span> with chef<h1>')
+                                .typeString('<h1 style="color:#DC2D55;font: normal normal 700 35px Poppins";>Curated menus.<h1>')
+                                .deleteAll()
+                                // .typeString('<h1 style="color: #DC2D55; font: normal normal 700 35px Poppins";>Curated meals that</h1>')
+                                // .typeString('<h1 style="color: #DC2D55; font: normal normal 700 35px Poppins";>fits in all pocket</h1>')
+                                // .typeString('<h1 style="color:#141923 ;font: normal normal 700 35px Poppins";>coming soon...</h1>')
+                                .start();
+
+                        }}
+                    /> */}
+                    <div className='textContainer'>
+
+
+                        <div className='textDiv'>
+                            {/* <div className="waviy">
+                                <span style={{ color: "rgb(225 217 212)", textShadow: "2px 2px #d96d13c2", fontSize: "40px" }}>Aquarium</span>
+                            </div> */}
+                            <div className="content">
+                                <h2>Aquarium</h2>
+                                <h2>Aquarium</h2>
+                            </div>
+                            <p>An aquarium is really just a spaceship for fish</p>
+                            <p>Aquarium fish make us realize how beautiful silence is</p>
+                            <p>I think life's a bit of what you make it and </p>
+                            <p>a little smidgen of you being the jellyfish,
+                            </p>
+                            <p>and the tide's just gently helping you along.</p>
+                        </div>
+
+
+                    </div>
 
                 </div>
+
+
                 <div className='main_card_container'>
 
                     <div className='description_card'>
 
-                       <div className='description_card_div'>
+                        <div className='description_card_div'>
                             <div className='description_card1'>
 
                                 <div className='description_card_img'>
@@ -107,9 +183,9 @@ function MainContainer() {
                             {/* <div style={{ backgroundImage: `url(${fish1})`, height: "100%", backgroundRepeat: "no-repeat", backgroundSize: "contain" }}></div> */}
 
                         </div>
-                        
-                       
- 
+
+
+
 
 
                     </div>
